c8857e6ba75475bd39648eb9a9797b8736f679b0
Alfredo Morales <alfredomorales@Alfredos-MacBook-Pro.local>
2023-12-15 21:59:29 -0600

    cleanup

--- src/components/sections/JarryHere.tsx
+++ src/components/sections/JarryHere.tsx
@@ -1,27 +1,16 @@
 import React from "react"
 import Box from "@mui/material/Box"
-import { Container, Grid, Typography, useMediaQuery, useTheme } from "@mui/material"
+import { Container, Grid, Typography } from "@mui/material"
 import { useTranslation } from "react-i18next"
 import { useSelector } from "react-redux"
 
-import { lightBlack } from "../../theme"
 import { getProfile } from "../../store/features/profileSlice"
 
 
-const imageStyle = {
-  width: "100%",
-  border: `0.5px solid ${lightBlack}`,
-  borderRadius: "4px",
-  opacity: 0.9
-}
-
-
 export default function AlfredoHere():JSX.Element {
-  const theme = useTheme()
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"))
   const profile = useSelector(getProfile)
 
-  const {i18n, t} = useTranslation("common")
+  const {i18n} = useTranslation("common")
 
   React.useEffect(() => {
     // switch to profile preferred language
